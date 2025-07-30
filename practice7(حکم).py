b5c4225a05321e5cb6eb22a3d1593116ea1b0ecf
AmiraliGhasemi7 <amirali464514@gmail.com>
2025-07-30 13:01:02 +0330

    حکم

---
+++ practice7(حکم).py
@@ -0,0 +1,337 @@
+import random
+numbers=[1,2,3,4,5,6,7,8,9,10,"king","queen","sarbaz"]
+symbols=["khesht","pick","heart","gishnise"]
+player1=[]
+player2=[]
+player3=[]
+player4=[]
+for i in range (13):
+    player1x=random.choice(numbers)
+    player1y=random.choice(symbols)
+    player1.append((player1x,player1y))
+print("player1:")
+print(player1)
+for i in range (13):
+    player2x=random.choice(numbers)
+    player2y=random.choice(symbols)
+    player2.append((player2x,player2y))
+print("player2:")
+print(player2)
+for i in range (13):
+    player3x=random.choice(numbers)
+    player3y=random.choice(symbols)
+    player3.append((player3x,player3y))
+print("player3:")
+print(player3)
+for i in range (13):
+    player4x=random.choice(numbers)
+    player4y=random.choice(symbols)
+    player4.append((player4x,player4y))
+print("player4:")
+print(player4)
+
+import random
+
+players = ["player1","player2","player3","player4"]
+ruler = random.choice(players)
+print(f"حاکم بازی: {ruler}")
+trump = input(f"{ruler} enter hokm bazi<khesht,pick,heart,gishniz:")
+print(f"حکم بازی: {trump}")
+rank_order = {'2': 2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, '10':10, 'J':11, 'Q':12, 'K':13, 'A':14}
+print("rank_order ='2': 2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9,'10':10, 'J':11, 'Q':12, 'K':13, 'A':14")
+print(input(("enter your card player1:")))
+print(input(("enter your card player2:")))
+print(input(("enter your card player3:")))
+print(input(("enter your card player4:")))
+print("game over:set1")
+print("لطفا خودت امتیازات رو محاسبه کن")
+
+import random
+numbers=[1,2,3,4,5,6,7,8,9,10,"king","queen","sarbaz"]
+symbols=["khesht","pick","heart","gishnise"]
+player1=[]
+player2=[]
+player3=[]
+player4=[]
+for i in range (13):
+    player1x=random.choice(numbers)
+    player1y=random.choice(symbols)
+    player1.append((player1x,player1y))
+print("player1:")
+print(player1)
+for i in range (13):
+    player2x=random.choice(numbers)
+    player2y=random.choice(symbols)
+    player2.append((player2x,player2y))
+print("player2:")
+print(player2)
+for i in range (13):
+    player3x=random.choice(numbers)
+    player3y=random.choice(symbols)
+    player3.append((player3x,player3y))
+print("player3:")
+print(player3)
+for i in range (13):
+    player4x=random.choice(numbers)
+    player4y=random.choice(symbols)
+    player4.append((player4x,player4y))
+print("player4:")
+print(player4)
+
+import random
+
+players = ["player1","player2","player3","player4"]
+ruler = random.choice(players)
+print(f"حاکم بازی: {ruler}")
+trump = input(f"{ruler} enter hokm bazi<khesht,pick,heart,gishniz:")
+print(f"حکم بازی: {trump}")
+rank_order = {'2': 2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, '10':10, 'J':11, 'Q':12, 'K':13, 'A':14}
+print("rank_order ='2': 2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9,'10':10, 'J':11, 'Q':12, 'K':13, 'A':14")
+print(input(("enter your card player1:")))
+print(input(("enter your card player2:")))
+print(input(("enter your card player3:")))
+print(input(("enter your card player4:")))
+print("game over:set2")
+print("لطفا خودت امتیازات رو محاسبه کن")
+
+import random
+numbers=[1,2,3,4,5,6,7,8,9,10,"king","queen","sarbaz"]
+symbols=["khesht","pick","heart","gishnise"]
+player1=[]
+player2=[]
+player3=[]
+player4=[]
+for i in range (13):
+    player1x=random.choice(numbers)
+    player1y=random.choice(symbols)
+    player1.append((player1x,player1y))
+print("player1:")
+print(player1)
+for i in range (13):
+    player2x=random.choice(numbers)
+    player2y=random.choice(symbols)
+    player2.append((player2x,player2y))
+print("player2:")
+print(player2)
+for i in range (13):
+    player3x=random.choice(numbers)
+    player3y=random.choice(symbols)
+    player3.append((player3x,player3y))
+print("player3:")
+print(player3)
+for i in range (13):
+    player4x=random.choice(numbers)
+    player4y=random.choice(symbols)
+    player4.append((player4x,player4y))
+print("player4:")
+print(player4)
+
+import random
+
+players = ["player1","player2","player3","player4"]
+ruler = random.choice(players)
+print(f"حاکم بازی: {ruler}")
+trump = input(f"{ruler} enter hokm bazi<khesht,pick,heart,gishniz:")
+print(f"حکم بازی: {trump}")
+rank_order = {'2': 2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, '10':10, 'J':11, 'Q':12, 'K':13, 'A':14}
+print("rank_order ='2': 2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9,'10':10, 'J':11, 'Q':12, 'K':13, 'A':14")
+print(input(("enter your card player1:")))
+print(input(("enter your card player2:")))
+print(input(("enter your card player3:")))
+print(input(("enter your card player4:")))
+print("game over:set3")
+print("لطفا خودت امتیازات رو محاسبه کن")
+
+import random
+numbers=[1,2,3,4,5,6,7,8,9,10,"king","queen","sarbaz"]
+symbols=["khesht","pick","heart","gishnise"]
+player1=[]
+player2=[]
+player3=[]
+player4=[]
+for i in range (13):
+    player1x=random.choice(numbers)
+    player1y=random.choice(symbols)
+    player1.append((player1x,player1y))
+print("player1:")
+print(player1)
+for i in range (13):
+    player2x=random.choice(numbers)
+    player2y=random.choice(symbols)
+    player2.append((player2x,player2y))
+print("player2:")
+print(player2)
+for i in range (13):
+    player3x=random.choice(numbers)
+    player3y=random.choice(symbols)
+    player3.append((player3x,player3y))
+print("player3:")
+print(player3)
+for i in range (13):
+    player4x=random.choice(numbers)
+    player4y=random.choice(symbols)
+    player4.append((player4x,player4y))
+print("player4:")
+print(player4)
+
+import random
+
+players = ["player1","player2","player3","player4"]
+ruler = random.choice(players)
+print(f"حاکم بازی: {ruler}")
+trump = input(f"{ruler} enter hokm bazi<khesht,pick,heart,gishniz:")
+print(f"حکم بازی: {trump}")
+rank_order = {'2': 2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, '10':10, 'J':11, 'Q':12, 'K':13, 'A':14}
+print("rank_order ='2': 2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9,'10':10, 'J':11, 'Q':12, 'K':13, 'A':14")
+print(input(("enter your card player1:")))
+print(input(("enter your card player2:")))
+print(input(("enter your card player3:")))
+print(input(("enter your card player4:")))
+print("game over:set4")
+print("لطفا خودت امتیازات رو محاسبه کن")
+
+import random
+numbers=[1,2,3,4,5,6,7,8,9,10,"king","queen","sarbaz"]
+symbols=["khesht","pick","heart","gishnise"]
+player1=[]
+player2=[]
+player3=[]
+player4=[]
+for i in range (13):
+    player1x=random.choice(numbers)
+    player1y=random.choice(symbols)
+    player1.append((player1x,player1y))
+print("player1:")
+print(player1)
+for i in range (13):
+    player2x=random.choice(numbers)
+    player2y=random.choice(symbols)
+    player2.append((player2x,player2y))
+print("player2:")
+print(player2)
+for i in range (13):
+    player3x=random.choice(numbers)
+    player3y=random.choice(symbols)
+    player3.append((player3x,player3y))
+print("player3:")
+print(player3)
+for i in range (13):
+    player4x=random.choice(numbers)
+    player4y=random.choice(symbols)
+    player4.append((player4x,player4y))
+print("player4:")
+print(player4)
+
+import random
+
+players = ["player1","player2","player3","player4"]
+ruler = random.choice(players)
+print(f"حاکم بازی: {ruler}")
+trump = input(f"{ruler} enter hokm bazi<khesht,pick,heart,gishniz:")
+print(f"حکم بازی: {trump}")
+rank_order = {'2': 2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, '10':10, 'J':11, 'Q':12, 'K':13, 'A':14}
+print("rank_order ='2': 2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9,'10':10, 'J':11, 'Q':12, 'K':13, 'A':14")
+print(input(("enter your card player1:")))
+print(input(("enter your card player2:")))
+print(input(("enter your card player3:")))
+print(input(("enter your card player4:")))
+print("game over:set5")
+print("لطفا خودت امتیازات رو محاسبه کن")
+
+import random
+numbers=[1,2,3,4,5,6,7,8,9,10,"king","queen","sarbaz"]
+symbols=["khesht","pick","heart","gishnise"]
+player1=[]
+player2=[]
+player3=[]
+player4=[]
+for i in range (13):
+    player1x=random.choice(numbers)
+    player1y=random.choice(symbols)
+    player1.append((player1x,player1y))
+print("player1:")
+print(player1)
+for i in range (13):
+    player2x=random.choice(numbers)
+    player2y=random.choice(symbols)
+    player2.append((player2x,player2y))
+print("player2:")
+print(player2)
+for i in range (13):
+    player3x=random.choice(numbers)
+    player3y=random.choice(symbols)
+    player3.append((player3x,player3y))
+print("player3:")
+print(player3)
+for i in range (13):
+    player4x=random.choice(numbers)
+    player4y=random.choice(symbols)
+    player4.append((player4x,player4y))
+print("player4:")
+print(player4)
+
+import random
+
+players = ["player1","player2","player3","player4"]
+ruler = random.choice(players)
+print(f"حاکم بازی: {ruler}")
+trump = input(f"{ruler} enter hokm bazi<khesht,pick,heart,gishniz:")
+print(f"حکم بازی: {trump}")
+rank_order = {'2': 2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, '10':10, 'J':11, 'Q':12, 'K':13, 'A':14}
+print("rank_order ='2': 2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9,'10':10, 'J':11, 'Q':12, 'K':13, 'A':14")
+print(input(("enter your card player1:")))
+print(input(("enter your card player2:")))
+print(input(("enter your card player3:")))
+print(input(("enter your card player4:")))
+print("game over:set6")
+print("لطفا خودت امتیازات رو محاسبه کن")
+
+import random
+numbers=[1,2,3,4,5,6,7,8,9,10,"king","queen","sarbaz"]
+symbols=["khesht","pick","heart","gishnise"]
+player1=[]
+player2=[]
+player3=[]
+player4=[]
+for i in range (13):
+    player1x=random.choice(numbers)
+    player1y=random.choice(symbols)
+    player1.append((player1x,player1y))
+print("player1:")
+print(player1)
+for i in range (13):
+    player2x=random.choice(numbers)
+    player2y=random.choice(symbols)
+    player2.append((player2x,player2y))
+print("player2:")
+print(player2)
+for i in range (13):
+    player3x=random.choice(numbers)
+    player3y=random.choice(symbols)
+    player3.append((player3x,player3y))
+print("player3:")
+print(player3)
+for i in range (13):
+    player4x=random.choice(numbers)
+    player4y=random.choice(symbols)
+    player4.append((player4x,player4y))
+print("player4:")
+print(player4)
+
+import random
+
+players = ["player1","player2","player3","player4"]
+ruler = random.choice(players)
+print(f"حاکم بازی: {ruler}")
+trump = input(f"{ruler} enter hokm bazi<khesht,pick,heart,gishniz:")
+print(f"حکم بازی: {trump}")
+rank_order = {'2': 2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, '10':10, 'J':11, 'Q':12, 'K':13, 'A':14}
+print("rank_order ='2': 2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9,'10':10, 'J':11, 'Q':12, 'K':13, 'A':14")
+print(input(("enter your card player1:")))
+print(input(("enter your card player2:")))
+print(input(("enter your card player3:")))
+print(input(("enter your card player4:")))
+print("game over:set7")
+print("لطفا خودت امتیازات رو محاسبه کن")
+
+print("finish")
